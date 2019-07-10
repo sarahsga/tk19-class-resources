7ef056a14aca56a9bb0f72164051a5b62f5af114
sarahsga <sarahsga4@gmail.com>
2019-07-10 06:19:14 +0500

    9th July 2019 class resources

---
+++ 2019-07-09/script.js
@@ -0,0 +1,126 @@
+let badStudent = [
+    "Kainat",
+    85,
+    64,
+    "teacher / web developer",
+    true
+]
+
+// let student1 = {
+//     name: "Samreen",
+//     age: 201,
+//     height: 9,
+//     profession: "housewife / web developer",
+//     isHappy: true,
+//     hobbies: ["makeup collection", "photoshop"]
+// };
+
+// let student2 = {
+//     name: "Somia",
+//     age: 420,
+//     height: 11,
+//     profession: "web developer",
+//     isHappy: true,
+//     hobbies: ["sleeping", "coding"]
+// };
+
+
+// Question: Create a data store for Tech Karo
+/*
+
+Homework
+
+- Create a data store for Tech Karo
+- Create a data store for Whatsapp
+
+*/
+
+let techkaro = {
+    name: "Tech Karo",
+    shortDescription: "A Web Dev Course",
+    longDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores animi, officia numquam ipsa repellat mollitia laboriosam minus provident commodi deserunt ad, cupiditate facilis! Amet reiciendis inventore molestiae omnis qui magnam.",
+    students: [
+        {
+            name: "Alaina",
+            age: 7001,
+            cnic: "42201-325783342-2",
+            email: "abc@xyz.com",
+            phoneNumbers: ["033127348325", "+92-214812967"],
+            group: "A",
+            gender: "F",
+            address: "dshgjsdh"
+        },
+        {
+            name: "Najam",
+            age: 7001,
+            cnic: "42201-325783342-2",
+            email: "abc@xyz.com",
+            phoneNumbers: ["033127348325", "+92-214812967"],
+            group: "A",
+            gender: "F",
+            address: "dshgjsdh"
+        },
+        {
+            name: "Farrukh",
+            age: 7001,
+            cnic: "42201-325783342-2",
+            email: "abc@xyz.com",
+            phoneNumbers: ["033127348325", "+92-214812967"],
+            group: "A",
+            gender: "F",
+            address: "dshgjsdh"
+        },
+    ],
+    fees: 500,
+    duration: 8,
+    sponsors: [
+        {
+            name: "Engro",
+            amount: 123456
+        }
+    ]
+
+};
+
+
+
+
+
+
+
+
+
+
+let students = [
+    {
+        name: "Samreen",
+        age: 201,
+        height: 9,
+        profession: "housewife / web developer",
+        isHappy: true,
+        hobbies: ["makeup collection", "photoshop"]
+    },
+    {
+        name: "Somia",
+        age: 420,
+        height: 11,
+        profession: "web developer",
+        isHappy: true,
+        hobbies: ["sleeping", "coding"]
+    }
+
+]
+
+// let student = [
+//     "drawing",
+//     "coding",   
+//     "poetry"
+// ]
+
+// let hobbies = [
+//     [ "decoration", "book reading"],
+//     ["makeup collection", "photoshop"]
+// ]
+
+
+// let students = 
